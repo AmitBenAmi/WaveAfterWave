80fe861552d3ef45cac86c14b359c50e70ad1e75
AmitBenAmi <benamiamit0@gmail.com>
2019-04-08 08:55:11 +0300

    Last

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_api import status
 from speechToText import textFromSpeech
 from textToSpeech import speechFromText
 from compressionConvert import convertTextToBinary
-from archiver import unzip
+from archiver import unzip, zip
 import json
 import os
 
@@ -15,7 +15,7 @@ def getTextFromSpeech():
         audio_file_data = request.data
 
         if not audio_file_data or audio_file_data is None:
-            return 'No empty files are allowed to convert to text\n', status.HTTP_400_BAD_REQUEST
+            return 'Empty files aren\'t allowed to convert to text\n', status.HTTP_400_BAD_REQUEST
         return textFromSpeech(audio_file_data)
     except Exception as ex:
         return str(ex) + '\n', status.HTTP_500_INTERNAL_SERVER_ERROR
@@ -26,7 +26,22 @@ def getCompressedFromSpeach():
         audio_file_data = request.data
 
         if not audio_file_data or audio_file_data is None:
-            return 'No empty files are allowed to convert to text\n', status.HTTP_400_BAD_REQUEST
+            return 'Empty files aren\'t allowed to convert to text\n', status.HTTP_400_BAD_REQUEST
+        compressedData = convertTextToBinary(text=textFromSpeech(audio_file_data))
+        
+        if compressedData is None:
+            return 'Error converting the data correctly, one word isn\'t the same as it was recordered\n', status.HTTP_500_INTERNAL_SERVER_ERROR
+        return compressedData
+    except Exception as ex:
+        return str(ex) + '\n', status.HTTP_500_INTERNAL_SERVER_ERROR
+
+@app.route('/api/yr', methods=['POST'])
+def getCompressedFromSpeach():
+    try:
+        audio_file_data = request.data
+
+        if not audio_file_data or audio_file_data is None:
+            return 'Empty files aren\'t allowed to convert to text\n', status.HTTP_400_BAD_REQUEST
         compressedData = convertTextToBinary(text=textFromSpeech(audio_file_data))
         
         if compressedData is None:
@@ -45,7 +60,7 @@ def getSpeechFromText():
             text = text.decode('utf-8')
 
         if not text:
-            return 'No empty strings are allowed to convert to speech\n', status.HTTP_400_BAD_REQUEST
+            return 'Empty strings aren\'t allowed to convert to speech\n', status.HTTP_400_BAD_REQUEST
         else:
             return speechFromText(text), status.HTTP_200_OK, {'Content-Type': 'audio/wav'}
     except Exception as ex:
@@ -57,8 +72,22 @@ def getFileFromArchive():
         archive_file = request.data
 
         if not archive_file or archive_file is None:
-            return 'No empty files are allowed to unarchive\n', status.HTTP_400_BAD_REQUEST
+            return 'Empty files aren\'t allowed to unarchive\n', status.HTTP_400_BAD_REQUEST
         data = unzip(archive_file)
+        if data is None:
+            return 'Error handling the archive', status.HTTP_400_BAD_REQUEST
+        return data
+    except Exception as ex:
+        return str(ex) + '\n', status.HTTP_500_INTERNAL_SERVER_ERROR
+
+@app.route('/api/aff', methods=['POST'])
+def getArchiveFromFile():
+    try:
+        file_to_archive = request.data
+
+        if not file_to_archive or file_to_archive is None:
+            return 'Empty files aren\'t allowed to archive\n', status.HTTP_400_BAD_REQUEST
+        data = zip(file_to_archive)
         if data is None:
             return 'Error handling the archive', status.HTTP_400_BAD_REQUEST
         return data
--- archiver.py
+++ archiver.py
@@ -1,17 +1,19 @@
 import zipfile, os, shutil, json
 
 extraction_dir_name = 'Extraction'
-zip_file_name = 'zipToExtract.zip'
+extract_zip_file_name = 'zipToExtract.zip'
+archive_zip_file_name = 'zipToArchive.zip'
+file_name_to_archive = 'file_to_archive'
 delimeter = '@@##$$%%^^&&'
 
 def unzip(zip_file):
-    deleteArchiveFile()
-    open(zip_file_name, 'wb+').write(zip_file)
+    deleteFile(extract_zip_file_name)
+    open(extract_zip_file_name, 'wb+').write(zip_file)
 
     deleteExtractionDirectory()
     os.mkdir(extraction_dir_name)
 
-    zip_ref = zipfile.ZipFile(zip_file_name, 'r')
+    zip_ref = zipfile.ZipFile(extract_zip_file_name, 'r')
     zip_ref.extractall(extraction_dir_name)
     zip_ref.close()
 
@@ -25,14 +27,25 @@ def unzip(zip_file):
             return None
         data.extend(extracted_file)
 
-    deleteArchiveFile()
+    deleteFile(extract_zip_file_name)
     deleteExtractionDirectory()
     return data
 
-def deleteArchiveFile():
-    if os.path.isfile(zip_file_name):
-        os.remove(zip_file_name)
+def deleteFile(file_name):
+    if os.path.isfile(file_name):
+        os.remove(file_name)
 
 def deleteExtractionDirectory():
     if os.path.isdir(extraction_dir_name):
         shutil.rmtree(extraction_dir_name)
+
+def zip(file_to_archive):
+    open(file_name_to_archive, 'wb+').write(file_to_archive)
+    zout = zipfile.ZipFile(archive_zip_file_name, 'w', zipfile.ZIP_DEFLATED)
+    zout.write(file_name_to_archive)
+    zout.close()
+    zip_data = open(archive_zip_file_name, 'rb').read()
+    deleteFile(archive_zip_file_name)
+    deleteFile(file_name_to_archive)
+
+    return zip_data
